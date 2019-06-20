09e31a9f9e392852a3c537f7064d16dfa0d2a662
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-06-19 22:30:57 -0700

    quick app

---
+++ js/counter.js
@@ -0,0 +1,41 @@
+'use strict'
+let num = 0;
+let number = document.getElementById('number');
+let inc = document.getElementById('inc');
+let dec = document.getElementById('dec');
+
+inc.addEventListener('click', increment);
+dec.addEventListener('click', decrement); 
+
+function increment(){
+    num++;
+    number.innerHTML = num;
+    numCheck();
+    localStorage.setItem('counter', num);
+    checkLocalStorage();
+  
+}
+function decrement (){
+    num--;
+    number.innerHTML = num;
+    numCheck();
+    localStorage.setItem('counter', num);
+    checkLocalStorage();
+  
+}
+
+function numCheck(){
+    if (num < 0) {
+        number.className = 'neg';
+    } else{
+        number.className = 'pos';
+    }
+}
+
+function checkLocalStorage(){
+    num = localStorage.getItem('counter');
+    number.innerHTML = num;
+    numCheck();
+}
+
+checkLocalStorage();
